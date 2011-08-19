08dae4ebceb06c7d32cb47a3d826bccb3097b479
dkandrov <dkandrov@7ca3a678-163c-e011-b84f-001e8c0267ff>
2011-08-19 08:20:13 +0000

    fix linux snapshot
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@1018 7ca3a678-163c-e011-b84f-001e8c0267ff

--- installer/linux/X11/create_bundle.sh
+++ installer/linux/X11/create_bundle.sh
@@ -157,7 +157,7 @@ add-plugin external_tool_support
 add-plugin dbi_sqlite
 add-plugin dbi_bam
 add-plugin assembly_browser
-add-plugin dbi_file
+#add-plugin dbi_file
 add-plugin ptools
 add-plugin dna_flexibility
 
