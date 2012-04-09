091512a247f80b18aa4b37f94a964319bed4d386
shutov <shutov@7ca3a678-163c-e011-b84f-001e8c0267ff>
2012-04-09 11:36:27 +0000

    GUITests: GTUtilsDialog: refactoring
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@1878 7ca3a678-163c-e011-b84f-001e8c0267ff

--- src/plugins/GUITestBase/src/GTUtilsDialog.cpp
+++ src/plugins/GUITestBase/src/GTUtilsDialog.cpp
@@ -38,10 +38,17 @@ namespace U2 {
 void GUIDialogWaiter::wait() {
     QWidget *widget = NULL;
 
-    if (type == Modal) {
-        widget = QApplication::activeModalWidget();
-    } else {
-        widget = QApplication::activePopupWidget();
+    switch (type) {
+        case Modal:
+            widget = QApplication::activeModalWidget();
+            break;
+
+        case Popup:
+            widget = QApplication::activePopupWidget();
+            break;
+
+        default:
+            break;
     }
 
     if (!widget) {
@@ -114,38 +121,16 @@ void GTUtilsDialog::CreateAnnotationDialogChecker::run() {
 #define GT_METHOD_NAME "run"
 void GTUtilsDialog::ExportProjectDialogChecker::run() {
 
-    QWidget* w = QApplication::activeModalWidget();
-    if (!w) {
-        return;
-    }
+    QWidget* dialog = QApplication::activeModalWidget();
+    GT_CHECK(dialog, "activeModalWidget is NULL");
 
-    QWidget* activeW = QApplication::focusWidget();
-    GT_CHECK(activeW != NULL, "Not export project dialog");
-    QObject* activeWP = activeW->parent();
-    GT_CHECK(activeWP != NULL, "Not export project dialog");
+    QLineEdit *projectFileLineEdit = qobject_cast<QLineEdit*>(GTWidget::findWidget(os, "projectFileEdit", dialog));
+    GT_CHECK(projectFileLineEdit != NULL, "LineEdit is NULL");
+    GT_CHECK(projectFileLineEdit->text() == projectName, "Project name is not " + projectName);
 
-    QList<QLineEdit*> lineEdits;
-    foreach (QObject *obj, w->children()) {
-        QLineEdit *l = qobject_cast<QLineEdit*>(obj);
-        if (l) {
-            lineEdits.push_front(l);
-        }
-    }
-
-    GT_CHECK(lineEdits.size() > 0, "There is no lineEdit in dialog");
-    GT_CHECK(lineEdits[0]->text() == projectName, "Project name is not " + projectName);
-
-    QList<QPushButton*> buttons;
-    foreach (QObject *obj, activeWP->children()) {
-        QPushButton *b = qobject_cast<QPushButton*>(obj);
-        if (b) {
-            buttons.push_front(b);
-        }
-    }
-
-    GT_CHECK(buttons.size() == 2, "There aren't 2 QPushButtons");
-
-    GTWidget::click(os, buttons[0]);
+    QAbstractButton *cancelButton = qobject_cast<QAbstractButton*>(GTWidget::findWidget(os, "CancelButton", dialog));
+    GT_CHECK(cancelButton != NULL, "CancelButton is NULL");
+    GTWidget::click(os, cancelButton);
 }
 #undef GT_METHOD_NAME
 #undef GT_CLASS_NAME
@@ -154,42 +139,22 @@ void GTUtilsDialog::ExportProjectDialogChecker::run() {
 #define GT_METHOD_NAME "run"
 void GTUtilsDialog::ExportProjectDialogFiller::run() {
 
-    QWidget* w = QApplication::activeModalWidget();
-    if (!w) {
-        return;
-    }
+    QWidget* dialog = QApplication::activeModalWidget();
+    GT_CHECK(dialog, "activeModalWidget is NULL");
 
-    QWidget* activeW = QApplication::focusWidget();
-    GT_CHECK(activeW != NULL, "Not export project dialog");
-    QObject* activeWP = activeW->parent();
-    GT_CHECK(activeWP != NULL, "Not export project dialog");
+    QLineEdit *projectFolderLineEdit = qobject_cast<QLineEdit*>(GTWidget::findWidget(os, "exportFolderEdit", dialog));
+    GT_CHECK(projectFolderLineEdit != NULL, "LineEdit is NULL");
+    GTLineEdit::setText(os, projectFolderLineEdit, projectFolder);
 
-    QList<QLineEdit*> lineEdits;
-    foreach (QObject *obj, w->children()) {
-        QLineEdit *l = qobject_cast<QLineEdit*>(obj);
-        if (l) {
-            lineEdits.push_front(l);
-        }
-    }
-
-    GT_CHECK(lineEdits.size() > 1, "There are no lineEdits in dialog");
+    QLineEdit *projectFileLineEdit = qobject_cast<QLineEdit*>(GTWidget::findWidget(os, "projectFileEdit", dialog));
+    GT_CHECK(projectFileLineEdit != NULL, "LineEdit is NULL");
     if (!projectName.isEmpty()) {
-        GTLineEdit::setText(os, lineEdits[0], projectName);
-    }
-    GTLineEdit::setText(os, lineEdits[1], projectFolder);
-
-
-    QList<QPushButton*> buttons;
-    foreach (QObject *obj, activeWP->children()) {
-        QPushButton *b = qobject_cast<QPushButton*>(obj);
-        if (b) {
-            buttons.push_front(b);
-        }
+        GTLineEdit::setText(os, projectFileLineEdit, projectName);
     }
 
-    GT_CHECK(buttons.size() == 2, "There aren't 2 QPushButtons");
-
-    GTWidget::click(os, buttons[1]);
+    QAbstractButton *okButton = qobject_cast<QAbstractButton*>(GTWidget::findWidget(os, "OKButton", dialog));
+    GT_CHECK(okButton != NULL, "CancelButton is NULL");
+    GTWidget::click(os, okButton);
 }
 #undef GT_METHOD_NAME
 #undef GT_CLASS_NAME
@@ -200,9 +165,7 @@ void GTUtilsDialog::MessageBoxDialogFiller::run() {
 
     QWidget* activeModal = QApplication::activeModalWidget();
     QMessageBox *messageBox = qobject_cast<QMessageBox*>(activeModal);
-    if (!messageBox) {
-        return;
-    }
+    GT_CHECK(messageBox != NULL, "messageBox is NULL");
 
     QAbstractButton* button = messageBox->button(b);
     GT_CHECK(button != NULL, "There is no such button in messagebox");
@@ -216,43 +179,20 @@ void GTUtilsDialog::MessageBoxDialogFiller::run() {
 #define GT_METHOD_NAME "run"
 void GTUtilsDialog::SaveProjectAsDialogFiller::run() {
 
-    QWidget* w = QApplication::activeModalWidget();
-    if (!w) {
-        return;
-    }
+    QWidget* dialog = QApplication::activeModalWidget();
+    GT_CHECK(dialog, "activeModalWidget is NULL");
 
-    QWidget* activeW = QApplication::focusWidget();
-    QObject* activeWP = activeW->parent();
-    QObject* activeWPP = activeWP->parent();
+    QLineEdit *projectNameEdit = qobject_cast<QLineEdit*>(GTWidget::findWidget(os, "projectNameEdit", dialog));
+    GTLineEdit::setText(os, projectNameEdit, projectName);
 
-    QList<QLineEdit*> lineEdits;
-    foreach (QObject *obj, activeWP->children()) {
-        QLineEdit *l = qobject_cast<QLineEdit*>(obj);
-        if (l) {
-            lineEdits.push_front(l);
-        }
-    }
+    QLineEdit *projectFolderEdit = qobject_cast<QLineEdit*>(GTWidget::findWidget(os, "projectFolderEdit", dialog));
+    GTLineEdit::setText(os, projectFolderEdit, projectFolder);
 
-    GT_CHECK(lineEdits.size() == 3, "There aren't 3 QLineEdits");
+    QLineEdit *projectFileEdit = qobject_cast<QLineEdit*>(GTWidget::findWidget(os, "projectFileEdit", dialog));
+    GTLineEdit::setText(os, projectFileEdit, projectFile);
 
-    GTLineEdit::setText(os, lineEdits[1], projectFile);
-    GTLineEdit::setText(os, lineEdits[2], projectName);
-    GTLineEdit::setText(os, lineEdits[0], projectFolder);
-
-    QList<QPushButton*> buttons;
-    foreach (QObject *obj, activeWPP->children()) {
-        QPushButton *b = qobject_cast<QPushButton*>(obj);
-        if (b) {
-            buttons.push_front(b);
-        }
-    }
-
-    GT_CHECK(buttons.size() == 2, "There aren't 2 QPushButtons");
-
-    QPushButton* cancelButton = buttons[0];
-    QPushButton* saveButton = buttons[1];
-
-    GTWidget::click(os, saveButton);
+    QAbstractButton *createButton = qobject_cast<QAbstractButton*>(GTWidget::findWidget(os, "createButton", dialog));
+    GTWidget::click(os, createButton);
 
     GTUtilsDialog::MessageBoxDialogFiller filler(os, QMessageBox::Yes);
     GTUtilsDialog::waitForDialog(os, &filler, GUIDialogWaiter::Modal, false); // MessageBox question appears only if there is already a file on a disk
@@ -270,59 +210,28 @@ void GTUtilsDialog::PopupChooser::run()
 #define GT_CLASS_NAME "GTUtilsDialog::RemoteDBDialogFiller"
 #define GT_METHOD_NAME "run"
 void GTUtilsDialog::RemoteDBDialogFiller::run() {
-    QWidget* w = QApplication::activeModalWidget();
-    if (!w) {
-        return;
-    }
 
-    QWidget* activeW = QApplication::focusWidget();
-    QObject* activeWP = activeW->parent();
-    QObject* activeWPP = activeWP->parent();
+    QWidget* dialog = QApplication::activeModalWidget();
+    GT_CHECK(dialog, "activeModalWidget is NULL");
 
-    QList<QLineEdit*> lineEdits;
-    foreach (QObject *obj, activeWP->children()) {
-        QLineEdit *l = qobject_cast<QLineEdit*>(obj);
-        if (l) {
-            lineEdits.push_front(l);
-        }
-    }
+    QLineEdit *idLineEdit = qobject_cast<QLineEdit*>(GTWidget::findWidget(os, "idLineEdit", dialog));
+    GTLineEdit::setText(os, idLineEdit, resID);
 
-    GT_CHECK(lineEdits.size() == 2, "There aren't 3 QLineEdits");
-
-    GTLineEdit::setText(os, lineEdits[1], resID);
+    QLineEdit *saveFilenameLineEdit = qobject_cast<QLineEdit*>(GTWidget::findWidget(os, "saveFilenameLineEdit", dialog));
     if(!saveDirPath.isEmpty()){
-        GTLineEdit::setText(os, lineEdits[0], saveDirPath);
+        GTLineEdit::setText(os, saveFilenameLineEdit, saveDirPath);
     }
 
-    QList<QComboBox*> comboBoxes;
-    foreach (QObject *obj, activeWP->children()) {
-        QComboBox *c = qobject_cast<QComboBox*>(obj);
-        if (c) {
-            comboBoxes.push_front(c);
-        }
-    }
-
-    GT_CHECK(comboBoxes.size() == 1, "There aren't 1 QComboBox");
-
-    GTComboBox::setCurrentIndex(os, comboBoxes.first(), DBItemNum);
-    
-    QList<QPushButton*> buttons;
-    foreach (QObject *obj, activeWP->children()) {
-        QPushButton *b = qobject_cast<QPushButton*>(obj);
-        if (b) {
-            buttons.push_front(b);
-        }
-    }
-
-    GT_CHECK(buttons.size() == 2, "There aren't 2 QPushButtons");
-
-    QPushButton* okButton = buttons[1];
-    QPushButton* cancelButton = buttons[0];
+    QComboBox *databasesBox = qobject_cast<QComboBox*>(GTWidget::findWidget(os, "databasesBox", dialog));
+    GTComboBox::setCurrentIndex(os, databasesBox, DBItemNum);
 
     if (pressCancel) {
-        GTWidget::click(os, cancelButton);
-    }else {
-        GTWidget::click(os, okButton);
+        QAbstractButton *rejectButton = qobject_cast<QAbstractButton*>(GTWidget::findWidget(os, "rejectButton", dialog));
+        GTWidget::click(os, rejectButton);
+    }
+    else {
+        QAbstractButton *acceptButton = qobject_cast<QAbstractButton*>(GTWidget::findWidget(os, "acceptButton", dialog));
+        GTWidget::click(os, acceptButton);
     }
 }
 #undef GT_METHOD_NAME
@@ -337,7 +246,6 @@ void GTUtilsDialog::ExportToSequenceFormatFiller::run()
 
     QLineEdit *lineEdit = dialog->findChild<QLineEdit*>();
     GT_CHECK(lineEdit != NULL, "line edit not found");
-
     GTLineEdit::setText(os, lineEdit, path + name);
 
     QRadioButton *button = dialog->findChild<QRadioButton*>(QString::fromUtf8("keepGapsRB"));
@@ -353,13 +261,11 @@ void GTUtilsDialog::ExportToSequenceFormatFiller::run()
         GTKeyboardDriver::keyClick(os, GTKeyboardDriver::key["space"]);
         break;
     }
+    GTGlobals::sleep(100);
 
     QPushButton *okButton = dialog->findChild<QPushButton*>(QString::fromUtf8("okButton"));
     GT_CHECK(okButton != NULL, "OK button not found");
-
-    GTGlobals::sleep(100);
     GTWidget::click(os, okButton);
-
 }
 #undef GT_METHOD_NAME
 #undef GT_CLASS_NAME
