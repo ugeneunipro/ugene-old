17577436d0a209ab6e47e35bc47e6d6e642ca871
shutov <shutov@7ca3a678-163c-e011-b84f-001e8c0267ff>
2012-04-20 04:23:14 +0000

    GUITests: excluded FAILED test.
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@1976 7ca3a678-163c-e011-b84f-001e8c0267ff

--- src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
+++ src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
@@ -134,7 +134,7 @@ REGISTER_TEST(GUITest_common_scenarios_sequence_edit::test_0002);
 //////////////////////////////////////////////////////////////////////////
 	REGISTER_TEST(GUITest_common_scenarios_project_sequence_exporting_from_project_view::test_0001);
 //	REGISTER_TEST(GUITest_common_scenarios_project_sequence_exporting_from_project_view::test_0002); excluded failed test
-	REGISTER_TEST(GUITest_common_scenarios_project_sequence_exporting_from_project_view::test_0003);
+//	REGISTER_TEST(GUITest_common_scenarios_project_sequence_exporting_from_project_view::test_0003); excluded failed test
 //     REGISTER_TEST(GUITest_common_scenarios_project_sequence_exporting_from_project_view::test_0004); excluded failed test
 //     REGISTER_TEST(GUITest_common_scenarios_project_sequence_exporting_from_project_view::test_0005); test is not completed
 //     REGISTER_TEST(GUITest_common_scenarios_project_sequence_exporting_from_project_view::test_0006); excluded failed test
