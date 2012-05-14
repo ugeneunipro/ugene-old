72cc11beabefcc157eb9163ca70ad53097fcc5c6
shutov <shutov@7ca3a678-163c-e011-b84f-001e8c0267ff>
2012-05-14 05:35:53 +0000

    test bugfix
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@2153 7ca3a678-163c-e011-b84f-001e8c0267ff

--- src/plugins/GUITestBase/src/tests/common_scenarios/sequence_edit/GTTestsSequenceEdit.cpp
+++ src/plugins/GUITestBase/src/tests/common_scenarios/sequence_edit/GTTestsSequenceEdit.cpp
@@ -237,7 +237,7 @@ GUI_TEST_CLASS_DEFINITION(test_0008) {
     CHECK_SET_ERR(sequenceBegin == "AAT", "Sequence starts with <" + sequenceBegin + ">, expected AAT");
 
     bool found = GTUtilsAnnotationsTreeView::findItem(os, "DUMMY_1", false);
-    CHECK_SET_ERR(found = true, "There is annotation DUMMY_1, expected state there is no annotation DUMMY_1");
+    CHECK_SET_ERR(found == true, "There is annotation DUMMY_1, expected state there is no annotation DUMMY_1");
 }
 
 GUI_TEST_CLASS_DEFINITION(test_0009) {
