e8df552a3d73f0e85601911e4e6e788f0fd5f45d
rkarasev <rkarasev@7ca3a678-163c-e011-b84f-001e8c0267ff>
2012-07-03 08:32:56 +0000

    added object name for action
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@2353 7ca3a678-163c-e011-b84f-001e8c0267ff

--- src/plugins/circular_view/src/CircularViewPlugin.cpp
+++ src/plugins/circular_view/src/CircularViewPlugin.cpp
@@ -101,6 +101,7 @@ void CircularViewContext::sl_sequenceWidgetAdded(ADVSequenceWidget* w) {
     }
     
     CircularViewAction* action = new CircularViewAction();
+    action->setObjectName("circular_view_action");
     action->setIcon(QIcon(":circular_view/images/circular.png"));
     action->setCheckable(true);
     action->setChecked(false);
