93b2ea5c8e3f0105a00ca2286f4f5dea291f379d
vmalin <vmalin@7ca3a678-163c-e011-b84f-001e8c0267ff>
2012-11-16 09:57:04 +0000

    Dotplot test
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@2925 7ca3a678-163c-e011-b84f-001e8c0267ff

---
+++ src/plugins/GUITestBase/src/tests/common_scenarios/dp_view/GTTestsDpView.cpp
@@ -0,0 +1,62 @@
+/**
+ * UGENE - Integrated Bioinformatics Tools.
+ * Copyright (C) 2008-2012 UniPro <ugene@unipro.ru>
+ * http://ugene.unipro.ru
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+#include "GTTestsDpView.h"
+#include "api/GTMouseDriver.h"
+#include "api/GTKeyboardDriver.h"
+#include "api/GTKeyboardUtils.h"
+#include "api/GTWidget.h"
+#include "api/GTFileDialog.h"
+#include "api/GTMenu.h"
+#include "api/GTTreeWidget.h"
+#include "api/GTGlobals.h"
+#include "GTUtilsApp.h"
+#include "GTUtilsDocument.h"
+#include "GTUtilsProjectTreeView.h"
+#include "GTUtilsAnnotationsTreeView.h"
+#include "GTUtilsSequenceView.h"
+#include "runnables/qt/PopupChooser.h"
+#include "runnables/ugene/corelibs/U2Gui/CreateAnnotationWidgetFiller.h"
+#include "runnables/ugene/corelibs/U2Gui/EditAnnotationDialogFiller.h"
+#include "runnables/ugene/corelibs/U2Gui/EditGroupAnnotationsDialogFiller.h"
+#include "runnables/ugene/plugins/dotplot/BuildDotPlotDialogFiller.h"
+#include "runnables/ugene/plugins/dotplot/DotPlotDialogFiller.h"
+
+namespace U2 {
+
+namespace GUITest_Common_scenarios_dp_view {
+
+GUI_TEST_CLASS_DEFINITION(test_0013) {
+
+    QMenu *menu;
+    menu=GTMenu::showMainMenu(os, MWMENU_TOOLS);
+
+    GTUtilsDialog::waitForDialog(os, new DotPlotFiller(os, 100));
+    Runnable *filler2 = new BuildDotPlotFiller(os, testDir + "_common_data/scenarios/dp_view/NC_014267.gb", "secondparametrTest",true,true,false,10);
+    GTUtilsDialog::waitForDialog(os, filler2);
+
+    GTMenu::clickMenuItem(os, menu, QStringList() << "Build dotplot");
+    GTGlobals::sleep();
+
+}
+} // namespace GUITest_Assembly_browser_
+} // namespace U2
+
+
