7d93b19c688f29cfec12fce56438265c8fdec86e
dkandrov <dkandrov@7ca3a678-163c-e011-b84f-001e8c0267ff>
2013-01-31 04:01:39 +0000

    Change year for trunk (UGENE-1392)
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@3366 7ca3a678-163c-e011-b84f-001e8c0267ff

--- src/plugins/external_tool_support/src/seqpos/SeqPosSupport.h
+++ src/plugins/external_tool_support/src/seqpos/SeqPosSupport.h
@@ -1,6 +1,6 @@
 /**
  * UGENE - Integrated Bioinformatics Tools.
- * Copyright (C) 2008-2012 UniPro <ugene@unipro.ru>
+ * Copyright (C) 2008-2013 UniPro <ugene@unipro.ru>
  * http://ugene.unipro.ru
  *
  * This program is free software; you can redistribute it and/or
@@ -42,7 +42,7 @@ private:
 #endif // _U2_SEQPOS_SUPPORT_
 /**
  * UGENE - Integrated Bioinformatics Tools.
- * Copyright (C) 2008-2012 UniPro <ugene@unipro.ru>
+ * Copyright (C) 2008-2013 UniPro <ugene@unipro.ru>
  * http://ugene.unipro.ru
  *
  * This program is free software; you can redistribute it and/or
