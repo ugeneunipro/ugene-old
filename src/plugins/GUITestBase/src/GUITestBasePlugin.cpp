78ee6b042e78ce79b8fec927632d73fee12da96f
atiunov <atiunov@7ca3a678-163c-e011-b84f-001e8c0267ff>
2013-03-12 10:55:27 +0000

    UGENE-1468 UGENE crashes when saving subalignment in msa. Turning on GUI test.
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@3665 7ca3a678-163c-e011-b84f-001e8c0267ff

--- src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
+++ src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
@@ -556,7 +556,7 @@ void GUITestBasePlugin::registerTests(GUITestBase *guiTestBase) {
     REGISTER_TEST(GUITest_common_scenarios_msa_editor_edit::test_0013_1);
     REGISTER_TEST(GUITest_common_scenarios_msa_editor_edit::test_0013_2);
 
-    REGISTER_TEST_IGNORED(GUITest_common_scenarios_msa_editor_edit::test_0014,"https://ugene.unipro.ru/tracker/browse/UGENE-1468");
+    REGISTER_TEST(GUITest_common_scenarios_msa_editor_edit::test_0014);
     REGISTER_TEST(GUITest_common_scenarios_msa_editor_edit::test_0015);
 //////////////////////////////////////////////////////////////////////////
 // Common scenarios/document_from_text/
