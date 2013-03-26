31347bc26b1edc549f4906dc708b7f1e651acfbe
vmalin <vmalin@7ca3a678-163c-e011-b84f-001e8c0267ff>
2013-03-26 04:32:36 +0000

    MSA editor test 0040
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@3783 7ca3a678-163c-e011-b84f-001e8c0267ff

--- src/plugins/GUITestBase/src/tests/common_scenarios/msa_editor/GTTestsMsaEditor.cpp
+++ src/plugins/GUITestBase/src/tests/common_scenarios/msa_editor/GTTestsMsaEditor.cpp
@@ -3474,12 +3474,15 @@ GUI_TEST_CLASS_DEFINITION(test_0040){//UGENE crashes when opening several files
     QFile human_T1(dataDir + "/samples/FASTA/human_T1.fa");
     human_T1.copy(dataDir + "/samples/CLUSTALW/human_T1.fa");
     GTFileDialog::openFileList(os,dataDir + "samples/CLUSTALW/",QStringList()<<"COI.aln"<<"human_T1.fa");
-    QFile(dataDir + "/samples/CLUSTALW/human_T1.fa").remove();
 
+    GTUtilsDialog::waitForDialog(os, new MessageBoxDialogFiller(os,QMessageBox::No));
     QTreeWidgetItem* human = GTUtilsProjectTreeView::findItem(os,"human_T1.fa");
     CHECK_SET_ERR(human, "human_T1.fa not added to project");
     QTreeWidgetItem* COI = GTUtilsProjectTreeView::findItem(os,"COI.aln");
     CHECK_SET_ERR(COI, "COI.aln not added to project");
+
+    QFile(dataDir + "/samples/CLUSTALW/human_T1.fa").remove();
+    GTGlobals::sleep();
 
 }
 
