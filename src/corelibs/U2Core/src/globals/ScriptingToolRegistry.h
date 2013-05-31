877602308abfbedf9618a1fce967ec5bd8e01f3f
vaskin <vaskin@7ca3a678-163c-e011-b84f-001e8c0267ff>
2013-05-31 03:50:22 +0000

    adding missing files
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@4084 7ca3a678-163c-e011-b84f-001e8c0267ff

---
+++ src/corelibs/U2Core/src/globals/ScriptingToolRegistry.h
@@ -0,0 +1,61 @@
+/**
+ * UGENE - Integrated Bioinformatics Tools.
+ * Copyright (C) 2008-2013 UniPro <ugene@unipro.ru>
+ * http://ugene.unipro.ru
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+
+#ifndef _U2_SCRIPTING_TOOL_REGISTRY_H
+#define _U2_SCRIPTING_TOOL_REGISTRY_H
+
+#include <U2Core/global.h>
+
+namespace U2 {
+
+class U2CORE_EXPORT ScriptingTool : public QObject {
+    Q_OBJECT
+public:
+    ScriptingTool(QString name, QString executablePath);
+
+    const QString getName() const {return name;}
+    const QString getPath() const {return path;}
+
+protected:
+    QString     name;
+    QString     path;
+
+};
+
+class U2CORE_EXPORT ScriptingToolRegistry : public QObject {
+    Q_OBJECT
+public:
+    ~ScriptingToolRegistry();
+
+    ScriptingTool* getByName(const QString& id);
+
+    bool registerEntry(ScriptingTool* t);
+    void unregisterEntry(const QString& id);
+
+    QList<ScriptingTool*> getAllEntries() const;
+
+protected:
+    QMap<QString, ScriptingTool*>    registry;
+
+}; 
+
+} //namespace
+#endif // _U2_SCRIPTING_TOOL_REGISTRY_H
---
+++ src/plugins/external_tool_support/src/python/PythonSupport.cpp
@@ -0,0 +1,84 @@
+/**
+ * UGENE - Integrated Bioinformatics Tools.
+ * Copyright (C) 2008-2013 UniPro <ugene@unipro.ru>
+ * http://ugene.unipro.ru
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+
+#include "PythonSupport.h"
+#include "ExternalToolSupportSettingsController.h"
+#include "ExternalToolSupportSettings.h"
+
+#include <U2Core/AppContext.h>
+#include <U2Core/AppSettings.h>
+#include <U2Core/ScriptingToolRegistry.h>
+#include <U2Core/U2SafePoints.h>
+#include <U2Core/U2OpStatusUtils.h>
+#include <U2Gui/MainWindow.h>
+#include <QtGui/QMainWindow>
+
+
+
+namespace U2 {
+
+
+PythonSupport::PythonSupport(const QString& name, const QString& path) : ExternalTool(name, path)
+{
+    if (AppContext::getMainWindow()) {
+        icon = QIcon(":external_tool_support/images/python.png");
+        grayIcon = QIcon(":external_tool_support/images/python_gray.png");
+        warnIcon = QIcon(":external_tool_support/images/python_warn.png");
+    }
+#ifdef Q_OS_WIN
+    executableFileName="python.exe";
+#else
+    #if defined(Q_OS_LINUX) || defined(Q_OS_MAC)
+    executableFileName="python";
+    #endif
+#endif
+    validMessage="Python ";
+    validationArguments << "--version";
+    
+    description+=tr("Python scripts interpreter");
+    versionRegExp=QRegExp("\\d+.\\d+.\\d+");
+    toolKitName="python";
+
+    connect(this, SIGNAL(si_pathChanged()), SLOT(sl_pathChanged())); 
+
+}
+
+void PythonSupport::sl_pathChanged(){
+
+    ScriptingToolRegistry* reg = AppContext::getScriptingToolRegistry();
+    if (reg){
+        if (isValid()){
+            if (reg->getByName(name) != NULL){
+                reg->unregisterEntry(name);
+            }
+            if (!path.isEmpty()){
+                ScriptingTool *pythonTool = new ScriptingTool(name, path);
+                reg->registerEntry(pythonTool);
+            }
+        }else{
+            reg->unregisterEntry(name);
+        }
+    }
+
+}
+
+
+}//namespace
