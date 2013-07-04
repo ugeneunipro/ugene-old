4c2cb29dc964b2aba5604724f0640cedb87b4b4a
atiunov <atiunov@7ca3a678-163c-e011-b84f-001e8c0267ff>
2013-07-04 12:32:22 +0000

    UGENE-1786: missed files (overwrited duiring mergeing).
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@4315 7ca3a678-163c-e011-b84f-001e8c0267ff

--- src/corelibs/U2Gui/src/OpenViewTask.cpp
+++ src/corelibs/U2Gui/src/OpenViewTask.cpp
@@ -309,15 +309,15 @@ LoadDASDocumentsAndOpenViewTask::LoadDASDocumentsAndOpenViewTask( const QString&
 : Task(tr("Load DAS documents and open view"), TaskFlags_NR_FOSCOE | TaskFlag_MinimizeSubtaskErrorText)
 ,accNumber(accId)
 ,fullpath(_fullPath)
-,featureSources(_featureSources)
 ,referenceSource(_referenceSource)
+,featureSources(_featureSources)
 ,loadDasDocumentTask(NULL)
 {
 
 }
 
 void LoadDASDocumentsAndOpenViewTask::prepare(){
-    loadDasDocumentTask = new LoadDASDocumentTask(accNumber, fullpath, referenceSource, featureSources);
+    loadDasDocumentTask = new ConvertIdAndLoadDASDocumentTask(accNumber, fullpath, referenceSource, featureSources);
 
     addSubTask(loadDasDocumentTask);
 }
