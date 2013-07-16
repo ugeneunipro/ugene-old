43b32e24b3ab2de5ed9eb1b0cacef2dd152784b6
atiunov <atiunov@7ca3a678-163c-e011-b84f-001e8c0267ff>
2013-07-16 11:40:18 +0000

    Gui test fix for mac.
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@4387 7ca3a678-163c-e011-b84f-001e8c0267ff

--- src/corelibs/U2Test/src/gui_tests/GUITest.cpp
+++ src/corelibs/U2Test/src/gui_tests/GUITest.cpp
@@ -2,7 +2,11 @@
 
 namespace U2 {
 
+#ifdef Q_OS_MAC
+const QString GUITest::testDir = "../../../../../../test/";
+#else
 const QString GUITest::testDir = "../../test/";
+#endif
 const QString GUITest::dataDir = "../../data/";
 
 } // namespace
