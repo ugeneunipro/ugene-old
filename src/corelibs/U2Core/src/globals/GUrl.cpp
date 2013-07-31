6a9ea3e74406dc7460fe8abedf72ac0e292f7296
raian <raian@7ca3a678-163c-e011-b84f-001e8c0267ff>
2013-07-31 07:39:50 +0000

    UGENE-1751
    UGENE triggers assert 
    
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@4453 7ca3a678-163c-e011-b84f-001e8c0267ff

--- src/corelibs/U2Core/src/globals/GUrl.cpp
+++ src/corelibs/U2Core/src/globals/GUrl.cpp
@@ -170,10 +170,17 @@ QString GUrl::baseFileName() const {
         QStringList args = urlString.split(U2_VFS_FILE_SEPARATOR, QString::SkipEmptyParts, Qt::CaseSensitive );
         if( 2 == args.size() ) {
             result = QFileInfo( args.at( 1 ) ).baseName();
+			if( !result.length() ) {
+				result = QFileInfo( args.at( 1 ) ).fileName();
+			}
         }
     } else {
         result = QFileInfo(path(this)).baseName();
+		if( !result.length() ) {
+			result = QFileInfo(path(this)).fileName();
+		}
     }
+	
     return result;
 }
 
