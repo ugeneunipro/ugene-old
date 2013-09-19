d8be2deee66691958ff3a6655bbb525f432e4ca0
kir <kir@7ca3a678-163c-e011-b84f-001e8c0267ff>
2013-09-19 13:03:39 +0000

    UGENE-1768 fix
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@4828 7ca3a678-163c-e011-b84f-001e8c0267ff

--- src/corelibs/U2Core/src/dbi/U2AbstractDbi.h
+++ src/corelibs/U2Core/src/dbi/U2AbstractDbi.h
@@ -109,7 +109,6 @@ protected:
     QHash<QString, QString>     metaInfo;
 };
 
-
 /** Default no-op implementation for write  methods of U2ObjectDbi */
 class U2SimpleObjectDbi : public U2ObjectDbi {
 protected:
@@ -168,46 +167,6 @@ public:
     }
 };
 
-/** Default no-op implementation for write  methods of U2SequenceDbi */
-class U2SimpleSequenceDbi: public U2SequenceDbi {
-protected:
-    U2SimpleSequenceDbi(U2Dbi* rootDbi) : U2SequenceDbi(rootDbi) {}
-
-public:
-    virtual void createSequenceObject(U2Sequence&, const QString&, U2OpStatus& os) {
-        U2DbiUtils::logNotSupported(U2DbiFeature_WriteSequence, getRootDbi(), os);
-    }
-
-    virtual void updateSequenceObject(U2Sequence& , U2OpStatus& os) {
-        U2DbiUtils::logNotSupported(U2DbiFeature_WriteSequence, getRootDbi(), os);
-    }
-
-    virtual void updateSequenceData(const U2DataId&, const U2Region&, const QByteArray&, const QVariantMap &, U2OpStatus& os) {
-        U2DbiUtils::logNotSupported(U2DbiFeature_WriteSequence, getRootDbi(), os);
-    }
-};
-
-
-/** Default no-op implementation for write  methods of U2MsaDbi */
-class U2SimpleMsaDbi : public U2MsaDbi {
-protected:
-    U2SimpleMsaDbi(U2Dbi* rootDbi) : U2MsaDbi(rootDbi) {}
-
-public:
-    virtual U2DataId createMsaObject(const QString&, const QString&, const U2AlphabetId&, U2OpStatus& os) {
-        U2DbiUtils::logNotSupported(U2DbiFeature_WriteMsa, getRootDbi(), os);
-    }
-    
-    virtual void removeRows(U2Msa&, const QList<qint64>& , U2OpStatus &os) {
-        U2DbiUtils::logNotSupported(U2DbiFeature_WriteMsa, getRootDbi(), os);
-    }
-
-    virtual void addRows(U2Msa&, QList<U2MsaRow>&, U2OpStatus& os) {
-        U2DbiUtils::logNotSupported(U2DbiFeature_WriteMsa, getRootDbi(), os);
-    }
-};
-
-
 /** Default no-op implementation for write  methods of U2AssemblyDbi */
 class U2SimpleAssemblyDbi: public U2AssemblyDbi{
 protected:
@@ -237,38 +196,6 @@ public:
     virtual void calculateCoverage(const U2DataId&, const U2Region&, U2AssemblyCoverageStat&, U2OpStatus& os) {
         U2DbiUtils::logNotSupported(U2DbiFeature_AssemblyCoverageStat, getRootDbi(), os);
     }
-};
-
-/** Default no-op implementation for write  methods of U2FeatureDbi */
-class U2SimpleFeatureDbi : public U2FeatureDbi {
-protected:
-    U2SimpleFeatureDbi(U2Dbi* rootDbi) : U2FeatureDbi(rootDbi) {}
-
-public:
-    virtual void createFeature(U2Feature&, const QList<U2FeatureKey>&, U2OpStatus& os) {
-        U2DbiUtils::logNotSupported(U2DbiFeature_WriteFeatures, getRootDbi(), os);
-    }
-
-    virtual void addKey(const U2DataId&, const U2FeatureKey&, U2OpStatus& os) {
-        U2DbiUtils::logNotSupported(U2DbiFeature_WriteFeatures, getRootDbi(), os);
-    }
-
-    virtual void removeAllKeys(const U2DataId&, const QString&, U2OpStatus& os) {
-        U2DbiUtils::logNotSupported(U2DbiFeature_WriteFeatures, getRootDbi(), os);
-    }
-
-    virtual void removeAllKeys(const U2DataId&, const U2FeatureKey&, U2OpStatus& os) {
-        U2DbiUtils::logNotSupported(U2DbiFeature_WriteFeatures, getRootDbi(), os);
-    }
-
-    virtual void updateLocation(const U2DataId&, const U2FeatureLocation&, U2OpStatus& os) {
-        U2DbiUtils::logNotSupported(U2DbiFeature_WriteFeatures, getRootDbi(), os);
-    }
-
-    virtual void removeFeature(const U2DataId&, U2OpStatus& os) {
-        U2DbiUtils::logNotSupported(U2DbiFeature_WriteFeatures, getRootDbi(), os);
-    }
-
 };
 
 /** Default no-op implementation for write  methods of U2AttributeDbi */
@@ -301,36 +228,6 @@ public:
         U2DbiUtils::logNotSupported(U2DbiFeature_WriteAttributes, getRootDbi(), os);
     }
 };
-
-class U2SimpleCrossDatabaseReferenceDbi: public U2CrossDatabaseReferenceDbi {
-public:
-    virtual U2CrossDatabaseReference getCrossReference(const U2DataId&, U2OpStatus& os) {
-        U2DbiUtils::logNotSupported(U2DbiFeature_WriteCrossDatabaseReferences, getRootDbi(), os);
-        return U2CrossDatabaseReference();
-    }
-
-    virtual void updateCrossReference(const U2CrossDatabaseReference&, U2OpStatus& os) {
-        U2DbiUtils::logNotSupported(U2DbiFeature_WriteCrossDatabaseReferences, getRootDbi(), os);
-    }
-};
-
-
-/** Default no-op implementation for write  methods of U2VariantDbi */
-class U2SimpleVariantDbi: public U2VariantDbi {
-protected:
-    U2SimpleVariantDbi(U2Dbi* rootDbi) : U2VariantDbi(rootDbi) {}
-
-public:
-    virtual void createVariantTrack(U2VariantTrack& , U2DbiIterator<U2Variant>* , const QString&, U2OpStatus& os) {
-        U2DbiUtils::logNotSupported(U2DbiFeature_WriteVariant, getRootDbi(), os); 
-    }
- 
-    virtual void updateVariantTrack(const U2VariantTrack&, U2OpStatus& os) {
-        U2DbiUtils::logNotSupported(U2DbiFeature_WriteVariant, getRootDbi(), os); 
-    }
-};
-
-
 
 }//namespace
 
