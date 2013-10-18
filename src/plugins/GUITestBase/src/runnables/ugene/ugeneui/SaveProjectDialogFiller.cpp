0f8983d26f1aa784c29f71cc2338814a41b976bc
vmalin <vmalin@7ca3a678-163c-e011-b84f-001e8c0267ff>
2013-10-18 03:07:49 +0000

    build fix
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@5123 7ca3a678-163c-e011-b84f-001e8c0267ff

---
+++ src/plugins/GUITestBase/src/runnables/ugene/ugeneui/SaveProjectDialogFiller.cpp
@@ -0,0 +1,37 @@
+/**
+ * UGENE - Integrated Bioinformatics Tools.
+ * Copyright (C) 2008-2013 UniPro <ugene@unipro.ru>
+ * http://ugene.unipro.ru
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+
+#include "SaveProjectDialogFiller.h"
+#include <QtGui/QApplication>
+#include <QtGui/QPushButton>
+
+#include "api/GTWidget.h"
+
+namespace U2{
+void SaveProjectDialogFiller::run(){
+    QWidget* dialog = QApplication::activeModalWidget();
+    CHECK_SET_ERR(dialog, "save project dialog not found");
+
+    QDialogButtonBox *buttonBox = qobject_cast<QDialogButtonBox *>(GTWidget::findWidget(os, "buttonBox",dialog));
+    QAbstractButton* no = buttonBox->button(QDialogButtonBox::No);
+    GTWidget::click(os, no);
+}
+}
