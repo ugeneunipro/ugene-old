541b03719f2250ba5e44b02450f0ceaf2056ddce
vporotov <vporotov@7ca3a678-163c-e011-b84f-001e8c0267ff>
2013-11-08 07:56:18 +0000

    UGENE-2379
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@5341 7ca3a678-163c-e011-b84f-001e8c0267ff

--- src/ugeneui/src/project_support/ProjectLoaderImpl.cpp
+++ src/ugeneui/src/project_support/ProjectLoaderImpl.cpp
@@ -25,7 +25,6 @@
 #include "MultipleDocumentsReadingModeSelectorController.h"
 #include "ProjectTasksGui.h"
 #include "ProjectImpl.h"
-#include "project_view/ProjectViewImpl.h"
 
 #include <U2Core/AddDocumentTask.h>
 #include <U2Core/Settings.h>
@@ -263,37 +262,6 @@ void ProjectLoaderImpl::updateRecentProjectsMenu() {
     }
 }
 
-namespace {
-/**
- * If there are only unsupported documents which are needed to load
- * then it is not needed to show the project because it will be empty
- */
-void prepareDocTab(const QList<AD2P_DocumentInfo> &docsInfo, const QList<AD2P_ProviderInfo> &docProviders) {
-    CHECK(docProviders.isEmpty(), );
-    foreach(const AD2P_DocumentInfo &info, docsInfo) {
-        const DocumentFormat *df = AppContext::getDocumentFormatRegistry()->getFormatById(info.formatId);
-        if (NULL == df) {
-            continue;
-        }
-        const GObjectType t = df->getSupportedObjectTypes().toList().first();
-        if (GObjectTypes::getTypeInfo(t).type != GObjectTypes::UNKNOWN) {
-            // the project will not be empty
-            return;
-        }
-    }
-
-    const MainWindow *mw = AppContext::getMainWindow();
-    CHECK(NULL != mw, );
-    MWDockManager *dm = mw->getDockManager();
-    CHECK(NULL != dm, );
-
-    { // do not activate the tab
-        dm->dontActivateNextTime(MWDockArea_Left);
-        AppContext::getSettings()->setValue(ProjectViewImpl::SETTINGS_ROOT + "firstShow", false);
-    }
-}
-}
-
 #define MAX_DOCS_TO_OPEN_VIEWS 5
 #define MAX_OBJECT_PER_DOC 5000000
 
@@ -459,8 +427,6 @@ Task* ProjectLoaderImpl::openWithProjectTask(const QList<GUrl>& _urls, const QVa
     if (docsInfo.isEmpty() && docProviders.isEmpty()) {
         return NULL;
     }
-
-    prepareDocTab(docsInfo, docProviders);
     return new AddDocumentsToProjectTask(docsInfo, docProviders);
 }
 
@@ -711,9 +677,9 @@ void ProjectDialogController::keyPressEvent(QKeyEvent* event) {
     int key = event->key();
     if (event->modifiers() == Qt::NoModifier && (key == Qt::Key_Enter || key == Qt::Key_Return)) {
         createButton->animateClick();
+    }else{
+        QDialog::keyPressEvent(event);
     }
-    QDialog::keyPressEvent(event);
-    
 }
 
 void ProjectDialogController::sl_folderSelectClicked() {
