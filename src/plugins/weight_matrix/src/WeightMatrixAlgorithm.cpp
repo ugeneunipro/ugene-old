bbdbc6d304cb52002d08ecbe35f179f19e5b0211
epushkova <epushkova@7ca3a678-163c-e011-b84f-001e8c0267ff>
2013-12-17 09:24:16 +0000

    UGENE-2575 fixed
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@5645 7ca3a678-163c-e011-b84f-001e8c0267ff

--- src/plugins/weight_matrix/src/WeightMatrixAlgorithm.cpp
+++ src/plugins/weight_matrix/src/WeightMatrixAlgorithm.cpp
@@ -34,13 +34,13 @@ float WeightMatrixAlgorithm::getScore(const char* seq, int len, const PWMatrix& 
     float curr = 0;
     if (m.getType() == PWM_MONONUCLEOTIDE) {
         for (int i = 0; i < len && i < l; i++) {       
-            char c = (complMap != NULL) ? complMapper[uchar(seq[i])] : seq[i];
+            char c = (complMap != NULL) ? complMapper[uchar(seq[len - i])] : seq[i];
             curr += m.getValue(DiProperty::index(c), i);
         }
     } else {
         for (int i = 0; i < len && i < l; i++) {       
-            char c1 = (complMap != NULL) ? complMapper[uchar(seq[i])] : seq[i];
-            char c2 = (complMap != NULL) ? complMapper[uchar(seq[i+1])] : seq[i+1];
+            char c1 = (complMap != NULL) ? complMapper[uchar(seq[len - i])] : seq[i];
+            char c2 = (complMap != NULL) ? complMapper[uchar(seq[len - (i+1)])] : seq[i+1];
             curr += m.getValue(DiProperty::index(c1, c2), i);
         }
     }
