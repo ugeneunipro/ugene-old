8ab6b6ac0e64ee21eb33bf97b8a131187e8375c8
iprotsyuk <iprotsyuk@7ca3a678-163c-e011-b84f-001e8c0267ff>
2014-06-02 13:12:01 +0000

    GUI test restored
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@6779 7ca3a678-163c-e011-b84f-001e8c0267ff

--- src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
+++ src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
@@ -272,7 +272,7 @@ void GUITestBasePlugin::registerTests(GUITestBase *guiTestBase) {
     REGISTER_TEST(GUITest_regression_scenarios::test_2284);
     REGISTER_TEST(GUITest_regression_scenarios::test_2316);
     //REGISTER_TEST_IGNORED(GUITest_regression_scenarios::test_2269, "impossible to catch expected error in opStatus without stopping test");
-    REGISTER_TEST_IGNORED(GUITest_regression_scenarios::test_2270, "https://ugene.unipro.ru/tracker/browse/UGENE-3025");
+    REGISTER_TEST(GUITest_regression_scenarios::test_2270);
     REGISTER_TEST(GUITest_regression_scenarios::test_2292);
     REGISTER_TEST(GUITest_regression_scenarios::test_2306);
     REGISTER_TEST(GUITest_regression_scenarios::test_2281);
