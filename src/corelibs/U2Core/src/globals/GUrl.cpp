336df82f47b48cdfcb32f566da40e20819d3fef8
atiunov <atiunov@7ca3a678-163c-e011-b84f-001e8c0267ff>
2014-06-03 07:52:28 +0000

    GUrl type checking fix: "file://some_file_path" is not recognized as network url type
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@6788 7ca3a678-163c-e011-b84f-001e8c0267ff

--- src/corelibs/U2Core/src/globals/GUrl.cpp
+++ src/corelibs/U2Core/src/globals/GUrl.cpp
@@ -97,7 +97,7 @@ GUrlType GUrl::getURLType(const QString& rawUrl) {
         result = GUrl_Http;
     } else if (rawUrl.startsWith("ftp://")) {
         result = GUrl_Ftp;
-    } else if (rawUrl.contains(QRegExp(".+:.*(/.*)?"))) {
+    } else if (!rawUrl.startsWith("file://") && rawUrl.contains(QRegExp(".+:.*(/.*)?"))) {
         return GUrl_Network;
     } else if( rawUrl.startsWith( U2_VFS_URL_PREFIX ) ) {
         result = GUrl_VFSFile;
