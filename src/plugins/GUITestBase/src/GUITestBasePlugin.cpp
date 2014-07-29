38b50119becd6c0fe33c288c83a5ab4f4fd1f74e
vmalin <vmalin@7ca3a678-163c-e011-b84f-001e8c0267ff>
2014-07-29 09:34:13 +0000

    tests restore
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@7354 7ca3a678-163c-e011-b84f-001e8c0267ff

--- src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
+++ src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
@@ -315,16 +315,16 @@ void GUITestBasePlugin::registerTests(GUITestBase *guiTestBase) {
     REGISTER_TEST(GUITest_regression_scenarios::test_2612);
     REGISTER_TEST(GUITest_regression_scenarios::test_2667);
     REGISTER_TEST(GUITest_regression_scenarios::test_2897);
-    REGISTER_TEST_IGNORED_LINUX(GUITest_regression_scenarios::test_2903, "https://ugene.unipro.ru/tracker/browse/UGENE-3205");
+    REGISTER_TEST(GUITest_regression_scenarios::test_2903);
 
     REGISTER_TEST(GUITest_regression_scenarios::test_3052);
     REGISTER_TEST(GUITest_regression_scenarios::test_3052_1);
     REGISTER_TEST(GUITest_regression_scenarios::test_3074);
     REGISTER_TEST(GUITest_regression_scenarios::test_3092);
     REGISTER_TEST(GUITest_regression_scenarios::test_3138);
-    REGISTER_TEST_IGNORED_LINUX(GUITest_regression_scenarios::test_3170, "https://ugene.unipro.ru/tracker/browse/UGENE-3199");
-    REGISTER_TEST_IGNORED_LINUX(GUITest_regression_scenarios::test_3209_1, "https://ugene.unipro.ru/tracker/browse/UGENE-3199");
-    REGISTER_TEST_IGNORED_LINUX(GUITest_regression_scenarios::test_3209_2, "https://ugene.unipro.ru/tracker/browse/UGENE-3199");
+    REGISTER_TEST(GUITest_regression_scenarios::test_3170);
+    REGISTER_TEST(GUITest_regression_scenarios::test_3209_1);
+    REGISTER_TEST(GUITest_regression_scenarios::test_3209_2);
 
 
 //////////////////////////////////////////////////////////////////////////
