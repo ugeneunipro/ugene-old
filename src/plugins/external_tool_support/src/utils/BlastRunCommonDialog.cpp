9d0b830a41fd3ba0ac7f251cf2f6326f76b18514
dkandrov <dkandrov@7ca3a678-163c-e011-b84f-001e8c0267ff>
2014-08-26 04:33:22 +0000

    Add new extentions for database files (UGENE-3432)
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@7513 7ca3a678-163c-e011-b84f-001e8c0267ff

--- src/plugins/external_tool_support/src/utils/BlastRunCommonDialog.cpp
+++ src/plugins/external_tool_support/src/utils/BlastRunCommonDialog.cpp
@@ -265,7 +265,7 @@ void BlastRunCommonDialog::sl_onBrowseDatabasePath(){
     lod.url = name = QFileDialog::getOpenFileName(NULL, tr("Select a database file"), lod.dir, "", NULL, options);
     if (!name.isEmpty()) {
         QFileInfo fileInfo(name);
-        baseNameLineEdit->setText(fileInfo.fileName().replace(QRegExp("(\\.\\d+)?(((formatDB|makeBlastDB)\\.log)|(\\.(phr|pin|psq|nhr|nin|nsq)))?$", Qt::CaseInsensitive), QString()));
+        baseNameLineEdit->setText(fileInfo.fileName().replace(QRegExp("(\\.\\d+)?(((formatDB|makeBlastDB)\\.log)|(\\.(phr|pin|psq|phd|pnd|pog|ppi|psi|phi|pni|ppd|psd|psq|pal|nhr|nin|nsq)))?$", Qt::CaseInsensitive), QString()));
         databasePathLineEdit->setText(fileInfo.dir().path());
     }
 }
