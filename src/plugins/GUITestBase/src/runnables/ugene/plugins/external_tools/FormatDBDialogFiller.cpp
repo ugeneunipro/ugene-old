884b58e41bc93936cd2e5ec03678c539e0da7522
ggrekhov <ggrekhov@7ca3a678-163c-e011-b84f-001e8c0267ff>
2014-09-15 03:47:41 +0000

    UGENE-3478: gui test [2]
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@7681 7ca3a678-163c-e011-b84f-001e8c0267ff

---
+++ src/plugins/GUITestBase/src/runnables/ugene/plugins/external_tools/FormatDBDialogFiller.cpp
@@ -0,0 +1,58 @@
+/**
+ * UGENE - Integrated Bioinformatics Tools.
+ * Copyright (C) 2008-2014 UniPro <ugene@unipro.ru>
+ * http://ugene.unipro.ru
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+
+#include "api/GTWidget.h"
+#include "api/GTSpinBox.h"
+#include "api/GTDoubleSpinBox.h"
+#include "api/GTCheckBox.h"
+#include "api/GTLineEdit.h"
+#include "api/GTComboBox.h"
+#include "api/GTRadioButton.h"
+#include <QtGui/QApplication>
+#include <QtGui/QGroupBox>
+#include <QtGui/QComboBox>
+
+#include "FormatDBDialogFiller.h"
+
+namespace U2 {
+
+FormatDBSupportRunDialogFiller::FormatDBSupportRunDialogFiller(U2OpStatus &os, const Parameters &parameters)
+: Filler(os, "FormatDBSupportRunDialog"), parameters(parameters)
+{
+
+}
+
+#define GT_CLASS_NAME "GTUtilsDialog::FormatDBSupportRunDialogFiller"
+#define GT_METHOD_NAME "run"
+
+void FormatDBSupportRunDialogFiller::run() {
+    QWidget *dialog = QApplication::activeModalWidget();
+    GT_CHECK(dialog, "activeModalWidget is NULL");
+
+    if (parameters.justCancel) {
+        GTUtilsDialog::clickButtonBox(os, dialog, QDialogButtonBox::Cancel);
+    }
+}
+
+#undef GT_METHOD_NAME
+#undef GT_CLASS_NAME
+
+} // U2
