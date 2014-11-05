d04fca0e5c0e864e81367670f6aa41e0d0760860
epushkova <epushkova@7ca3a678-163c-e011-b84f-001e8c0267ff>
2014-11-05 08:58:09 +0000

    Unignore test
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@8149 7ca3a678-163c-e011-b84f-001e8c0267ff

--- src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
+++ src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
@@ -1406,7 +1406,7 @@ void GUITestBasePlugin::registerTests(GUITestBase *guiTestBase) {
     REGISTER_TEST(GUITest_common_scenarios_shared_database::cm_test_0007);
     REGISTER_TEST(GUITest_common_scenarios_shared_database::cm_test_0008);
     REGISTER_TEST(GUITest_common_scenarios_shared_database::cm_test_0009);
-    REGISTER_TEST_IGNORED(GUITest_common_scenarios_shared_database::cm_test_0013, "https://ugene.unipro.ru/tracker/browse/UGENE-3340");
+    REGISTER_TEST(GUITest_common_scenarios_shared_database::cm_test_0013);
 
     REGISTER_TEST(GUITest_common_scenarios_shared_database::proj_test_0001);
     REGISTER_TEST(GUITest_common_scenarios_shared_database::proj_test_0002);
