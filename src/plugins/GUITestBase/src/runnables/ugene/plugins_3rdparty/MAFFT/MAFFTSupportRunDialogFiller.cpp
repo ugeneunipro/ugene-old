e055bdd6640ab1d219421da4cee80cb62a8c4968
kir <kir@7ca3a678-163c-e011-b84f-001e8c0267ff>
2014-11-20 05:48:19 +0000

    GUI tests added
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@8333 7ca3a678-163c-e011-b84f-001e8c0267ff

---
+++ src/plugins/GUITestBase/src/runnables/ugene/plugins_3rdparty/MAFFT/MAFFTSupportRunDialogFiller.cpp
@@ -0,0 +1,72 @@
+/**
+ * UGENE - Integrated Bioinformatics Tools.
+ * Copyright (C) 2008-2014 UniPro <ugene@unipro.ru>
+ * http://ugene.unipro.ru
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+ 
+#include "MAFFTSupportRunDialogFiller.h"
+#include "api/GTWidget.h"
+#include "api/GTSpinBox.h"
+#include "api/GTDoubleSpinBox.h"
+#include "api/GTCheckBox.h"
+#include "api/GTLineEdit.h"
+#include "api/GTComboBox.h"
+#include "api/GTRadioButton.h"
+#include <QtGui/QApplication>
+#include <QtGui/QGroupBox>
+#include <QtGui/QComboBox>
+
+namespace U2 {
+
+#define GT_CLASS_NAME "GTUtilsDialog::MAFFTSupportRunDialogFiller"
+#define GT_METHOD_NAME "run"
+
+void MAFFTSupportRunDialogFiller::run() {
+    QWidget* dialog = QApplication::activeModalWidget();
+    GT_CHECK(dialog, "activeModalWidget is NULL");
+    
+    QCheckBox* gapOpenCheckBox = qobject_cast<QCheckBox*>(GTWidget::findWidget(os, "gapOpenCheckBox", dialog));
+    GT_CHECK(gapOpenCheckBox, "gapOpenCheckBox is NULL");
+    GTCheckBox::setChecked(os, gapOpenCheckBox, parameters->ckeckBox_gapOpenCheckBox_checked);
+
+    QDoubleSpinBox* gapOpenSpinBox = qobject_cast<QDoubleSpinBox*>(GTWidget::findWidget(os, "gapOpenSpinBox", dialog));
+    GT_CHECK(gapOpenSpinBox, "gapOpenSpinBox is NULL");
+    GTDoubleSpinbox::setValue(os, gapOpenSpinBox, parameters->doubleSpin_gapOpenSpinBox_value);
+
+    QCheckBox* gapExtCheckBox = qobject_cast<QCheckBox*>(GTWidget::findWidget(os, "gapExtCheckBox", dialog));
+    GT_CHECK(gapExtCheckBox, "gapExtCheckBox is NULL");
+    GTCheckBox::setChecked(os, gapExtCheckBox, parameters->ckeckBox_gapExtCheckBox_checked);
+
+    QDoubleSpinBox* gapExtSpinBox = qobject_cast<QDoubleSpinBox*>(GTWidget::findWidget(os, "gapExtSpinBox", dialog));
+    GT_CHECK(gapExtSpinBox, "gapExtSpinBox is NULL");
+    GTDoubleSpinbox::setValue(os, gapExtSpinBox, parameters->doubleSpin_gapExtSpinBox_value);
+
+    QCheckBox* maxNumberIterRefinementCheckBox = qobject_cast<QCheckBox*>(GTWidget::findWidget(os, "maxNumberIterRefinementCheckBox", dialog));
+    GT_CHECK(maxNumberIterRefinementCheckBox, "maxNumberIterRefinementCheckBox is NULL");
+    GTCheckBox::setChecked(os, maxNumberIterRefinementCheckBox, parameters->ckeckBox_maxNumberIterRefinementCheckBox_checked);
+
+    QSpinBox* maxNumberIterRefinementSpinBox = qobject_cast<QSpinBox*>(GTWidget::findWidget(os, "maxNumberIterRefinementSpinBox", dialog));
+    GT_CHECK(maxNumberIterRefinementSpinBox, "maxNumberIterRefinementSpinBox is NULL");
+    GTSpinBox::setValue(os, maxNumberIterRefinementSpinBox, parameters->spin_maxNumberIterRefinementSpinBox_value);
+
+    GTWidget::click(os, GTWidget::findButtonByText(os, "Align", dialog));
+}
+#undef GT_METHOD_NAME
+#undef GT_CLASS_NAME
+
+}
---
+++ src/plugins/GUITestBase/src/runnables/ugene/plugins_3rdparty/MAFFT/MAFFTSupportRunDialogFiller.h
@@ -0,0 +1,69 @@
+/**
+ * UGENE - Integrated Bioinformatics Tools.
+ * Copyright (C) 2008-2014 UniPro <ugene@unipro.ru>
+ * http://ugene.unipro.ru
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+ 
+#ifndef _U2_GUI_MAFFT_SUPPORT_RUN_DIALOG_FILLER_H_
+#define _U2_GUI_MAFFT_SUPPORT_RUN_DIALOG_FILLER_H_
+ 
+#include "GTUtilsDialog.h"
+#include "api/GTFileDialog.h"
+
+namespace U2 {
+
+class MAFFTSupportRunDialogFiller : public Filler {
+public:
+    class Parameters {
+        public:
+        Parameters():
+            ckeckBox_gapOpenCheckBox_checked(0),
+            doubleSpin_gapOpenSpinBox_value(1.53),
+            ckeckBox_gapExtCheckBox_checked(0),
+            doubleSpin_gapExtSpinBox_value(0),
+            ckeckBox_maxNumberIterRefinementCheckBox_checked(0),
+            spin_maxNumberIterRefinementSpinBox_value(0){}
+
+        bool ckeckBox_gapOpenCheckBox_checked;
+        double doubleSpin_gapOpenSpinBox_value;
+        bool ckeckBox_gapExtCheckBox_checked;
+        double doubleSpin_gapExtSpinBox_value;
+        bool ckeckBox_maxNumberIterRefinementCheckBox_checked;
+        int spin_maxNumberIterRefinementSpinBox_value;
+    };
+
+
+    MAFFTSupportRunDialogFiller(U2OpStatus &os, Parameters* parameters) :
+        Filler(os, "MAFFTSupportRunDialog"),
+        parameters(parameters) {
+            CHECK_SET_ERR(parameters, "Invalid filler parameters: NULL pointer");
+    }
+
+virtual void run();
+
+private:
+
+Parameters* parameters;
+
+};
+
+}
+
+#endif
+
+ 
