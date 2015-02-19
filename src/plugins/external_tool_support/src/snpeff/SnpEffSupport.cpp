ab4e56513ac02a48387b62f88465dcae6ab65279
kir <kir@7ca3a678-163c-e011-b84f-001e8c0267ff>
2015-02-19 20:51:35 +0000

    snpEff updated
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@9298 7ca3a678-163c-e011-b84f-001e8c0267ff

--- src/plugins/external_tool_support/src/snpeff/SnpEffSupport.cpp
+++ src/plugins/external_tool_support/src/snpeff/SnpEffSupport.cpp
@@ -44,7 +44,7 @@ SnpEffSupport::SnpEffSupport(const QString& name, const QString& path) : Externa
 
     executableFileName="snpEff.jar";
 
-    validMessage = "Usage: snpEff \\[eff\\] \\[options\\] genome_version \\[input_file\\]";
+    validMessage = "Usage: snpEff \\[command\\] \\[options\\] \\[files\\]";
     description = tr("<i>SnpEff</i>: Genetic variant annotation and effect prediction toolbox.");
 
     versionRegExp = QRegExp("version SnpEff (\\d+.\\d+)");
