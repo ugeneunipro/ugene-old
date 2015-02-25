c68449e6d24ee771e2382f1b0407837a27906a2e
kir <kir@7ca3a678-163c-e011-b84f-001e8c0267ff>
2015-02-25 08:00:27 +0000

    fix for GUI test
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@9347 7ca3a678-163c-e011-b84f-001e8c0267ff

--- src/plugins/GUITestBase/src/tests/common_scenarios/options_panel/sequence_view/GTTestsOptionPanelSequenceView.cpp
+++ src/plugins/GUITestBase/src/tests/common_scenarios/options_panel/sequence_view/GTTestsOptionPanelSequenceView.cpp
@@ -119,7 +119,7 @@ GUI_TEST_CLASS_DEFINITION(test_0004){
     GTUtilsTaskTreeView::waitTaskFinished(os);
     GTGlobals::sleep();
     GTWidget::click(os, GTWidget::findWidget(os, "annotations_tree_widget"));
-    GTUtilsAnnotationsTreeView::findItem(os, "misc_feature  (0, 2)");
+    GTUtilsAnnotationsTreeView::findItem(os, "Misc. Feature  (0, 2)");
 }
 
 GUI_TEST_CLASS_DEFINITION(test_0005){
