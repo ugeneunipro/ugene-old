ecea51c44dccbca53e02b92c147629c438f865c1
atiunov <atiunov@7ca3a678-163c-e011-b84f-001e8c0267ff>
2015-04-06 12:35:56 +0000

    GUITest_common_scenarios_primer_library_test_0007 fix
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@9809 7ca3a678-163c-e011-b84f-001e8c0267ff

--- src/plugins/GUITestBase/src/tests/common_scenarios/pcr/GTTestsPrimerLibrary.cpp
+++ src/plugins/GUITestBase/src/tests/common_scenarios/pcr/GTTestsPrimerLibrary.cpp
@@ -353,8 +353,8 @@ GUI_TEST_CLASS_DEFINITION(test_0007) {
     CHECK_SET_ERR(items.size() == 2, QString("Unexpected annotations count: epxect %1, got %2").arg(2).arg(items.size()));
 
     const QString sequenceQualifier = GTUtilsAnnotationsTreeView::getQualifierValue(os, "sequence", "primer_bind");
-    const QString gcQualifier = GTUtilsAnnotationsTreeView::getQualifierValue(os, "GC%", "primer_bind");
-    const QString tmQualifier = GTUtilsAnnotationsTreeView::getQualifierValue(os, "Tm", "primer_bind");
+    const QString gcQualifier = GTUtilsAnnotationsTreeView::getQualifierValue(os, "gc%", "primer_bind");
+    const QString tmQualifier = GTUtilsAnnotationsTreeView::getQualifierValue(os, "tm", "primer_bind");
 
     CHECK_SET_ERR("AAAA" == sequenceQualifier, QString("Incorrect value of sequence qualifier: '%1'").arg(sequenceQualifier));
     CHECK_SET_ERR("0" == gcQualifier, QString("Incorrect value of gc content qualifier: '%1'").arg(gcQualifier));
