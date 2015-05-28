41a11ccb71b0ce13ccafcc04f8f2182870a91933
atiunov <atiunov@7ca3a678-163c-e011-b84f-001e8c0267ff>
2015-05-28 10:57:01 +0000

    UGENE-339: GUI test fix: test is disabled for non-Mac systems
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@10302 7ca3a678-163c-e011-b84f-001e8c0267ff

--- src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
+++ src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
@@ -147,7 +147,7 @@ void GUITestBasePlugin::registerTests(GUITestBase *guiTestBase) {
     REGISTER_TEST(GUITest_regression_scenarios::test_0073_1);
     REGISTER_TEST(GUITest_regression_scenarios::test_0073_2);
 
-    REGISTER_TEST(GUITest_regression_scenarios::test_0339);
+    REGISTER_TEST_MAC(GUITest_regression_scenarios::test_0339, "Mac specific test");
 
     REGISTER_TEST(GUITest_regression_scenarios::test_0407);
     REGISTER_TEST(GUITest_regression_scenarios::test_0490);
