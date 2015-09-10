2da8ece941eda5fbdd7f7224b34a0c5166d264c9
asavchenko <asavchenko@7ca3a678-163c-e011-b84f-001e8c0267ff>
2015-09-10 07:11:26 +0000

    UGENE-3101 GUI test scenario fix
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@10949 7ca3a678-163c-e011-b84f-001e8c0267ff

--- src/plugins/GUITestBase/src/tests/regression_scenarios/GTTestsRegressionScenarios_3001_4000.cpp
+++ src/plugins/GUITestBase/src/tests/regression_scenarios/GTTestsRegressionScenarios_3001_4000.cpp
@@ -580,8 +580,11 @@ GUI_TEST_CLASS_DEFINITION(test_3092) {
 }
 
 GUI_TEST_CLASS_DEFINITION(test_3101){
-    //    1. Open the data/samples/CLUSTALW/COI.aln
+    //1. Open "_common_data\scenarios\_regression\3101\enzymes"
+    //Expected state : "Document format selection" dialog appeared
     GTFileDialog::openFile(os, testDir + "_common_data/scenarios/_regression/3101", "enzymes");
+    //2. Select "Newick" format
+    //Expected state : Task finished with error, but without assert call
     GTUtilsDialog::waitForDialog(os, new DocumentFormatSelectorDialogFiller(os, "Newick Standard"));
 }
 
