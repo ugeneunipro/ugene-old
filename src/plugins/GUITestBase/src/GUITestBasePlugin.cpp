c559448517ae92f5bf909b296635cedcd70201b1
kir <kir@7ca3a678-163c-e011-b84f-001e8c0267ff>
2015-10-08 10:10:33 +0000

    GUI test Ignored
    git-svn-id: https://local.ugene.unipro.ru/svn/ugene/trunk@11159 7ca3a678-163c-e011-b84f-001e8c0267ff

--- src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
+++ src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
@@ -1199,7 +1199,7 @@ void GUITestBasePlugin::registerTests(GUITestBase *guiTestBase) {
     REGISTER_TEST(GUITest_common_scenarios_project::test_0036);
     REGISTER_TEST(GUITest_common_scenarios_project::test_0037);
     REGISTER_TEST(GUITest_common_scenarios_project::test_0038);
-    REGISTER_TEST(GUITest_common_scenarios_project::test_0038_1);
+    REGISTER_TEST_IGNORED(GUITest_common_scenarios_project::test_0038_1, "UGENE-4434");
     REGISTER_TEST(GUITest_common_scenarios_project::test_0039);
     REGISTER_TEST(GUITest_common_scenarios_project::test_0040);
     REGISTER_TEST_NOT_FOR_MAC(GUITest_common_scenarios_project::test_0041);
